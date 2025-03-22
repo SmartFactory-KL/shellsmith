33f7955ff4344415e2dc4089dc5f44cb7cb615ac
Peter Stein <peter.stein@dfki.de>
2025-03-22 17:08:17 +0100

    Add upload test cases

--- src/shellsmith/cli/commands/shell.py
+++ src/shellsmith/cli/commands/shell.py
@@ -1,11 +1,16 @@
+import requests.exceptions
+
 from shellsmith import crud, services
 
 
 def shell_delete(shell_id: str, cascade: bool = False):
     print(f"🗑️ Deleting Shell: {shell_id}")
-    if cascade:
-        services.delete_shell_cascading(shell_id)
-        print(f"✅ Shell '{shell_id}' and its submodels deleted.")
-    else:
-        crud.delete_shell(shell_id)
-        print(f"✅ Shell '{shell_id}' deleted.")
+    try:
+        if cascade:
+            services.delete_shell_cascading(shell_id)
+            print(f"✅ Shell '{shell_id}' and its submodels deleted.")
+        else:
+            crud.delete_shell(shell_id)
+            print(f"✅ Shell '{shell_id}' deleted.")
+    except requests.exceptions.HTTPError as e:
+        print(f"❌ Failed to delete Shell '{shell_id}': {e}")
