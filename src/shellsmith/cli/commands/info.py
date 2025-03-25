ad40d48294887d19bdcad45e3f9c2a7dca286b54
Peter Stein <peter.stein@dfki.de>
2025-03-25 21:22:13 +0100

    Fix `KeyError` for missing `idShort`
    Handled cases where `idShort` is missing in Asset Administration Shells and Submodels.
    
    According to the "Asset Administration Shell Specification – Part 1: Metamodel (V3.0)", `idShort` is _optional_ for `AssetAdministrationShell` (see p. 29) and _recommended_ (see p. 30) for `Submodel`.
    
    > Note: in version V1.0 of this specification, idShort was optional for identifiables. This changed in V2.0: idShort was set to mandatory for all referables. With V3.0, idShort was again made optional
    
    Spec reference:
    https://industrialdigitaltwin.org/wp-content/uploads/2023/04/IDTA-01001-3-0_SpecificationAssetAdministrationShell_Part1_Metamodel.pdf

--- src/shellsmith/cli/commands/info.py
+++ src/shellsmith/cli/commands/info.py
@@ -17,21 +17,25 @@ def print_unreferenced_submodels():
         print("⚠️ Unreferenced Submodels:")
         for submodel_id in submodel_ids:
             submodel = shellsmith.get_submodel(submodel_id)
-            id_short = submodel["idShort"]
+            id_short = submodel.get("idShort", "<no idShort>")
             print(f"- {id_short} ({submodel_id})")
 
 
 def print_shells_tree():
     shells = shellsmith.get_shells()
     for shell in shells:
-        print(f"{shell['idShort']}: {shell['id']}")
+        shell_id = shell['id']
+        shell_id_short = shell.get('idShort', "<no idShort>")
+        print(f"{shell_id_short}: {shell_id}")
 
-        submodels = services.get_shell_submodels(shell["id"])
+        submodels = services.get_shell_submodels(shell_id)
 
         for i, submodel in enumerate(submodels):
             is_last = i == len(submodels) - 1
             prefix = "└──" if is_last else "├──"
-            print(f"{prefix} {submodel['idShort']}: {submodel['id']}")
+            submodel_id = submodel['id']
+            submodel_id_short = submodel.get('idShort', "<no idShort>")
+            print(f"{prefix} {submodel_id}: {submodel_id_short}")
 
 
 def print_dangling_submodel_refs():
@@ -42,6 +46,7 @@ def print_dangling_submodel_refs():
         print("⚠️ Dangling Submodel References:")
         for shell_id, submodel_ids in dangling.items():
             shell = shellsmith.get_shell(shell_id)
-            print(f"- {shell['idShort']}: {shell_id}")
+            shell_id_short = shell.get('idShort', "<no idShort>")
+            print(f"- {shell_id_short}: {shell_id}")
             for submodel_id in submodel_ids:
                 print(f"  └── {submodel_id}")
