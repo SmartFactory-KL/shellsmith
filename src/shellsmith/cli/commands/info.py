8c36ed2247ae74e9a7193b722ee12996f0302337
Peter Stein <peter.stein@dfki.de>
2025-03-25 22:13:21 +0100

    Consistently print "<id_short>: <id>"
    Instead of <id> (<id_short>)

--- src/shellsmith/cli/commands/info.py
+++ src/shellsmith/cli/commands/info.py
@@ -3,7 +3,6 @@ from shellsmith import services
 
 
 def info():
-    print("ℹ️ Showing all shells")
     print_shells_tree()
     print_unreferenced_submodels()
     print_dangling_submodel_refs()
@@ -18,14 +17,14 @@ def print_unreferenced_submodels():
         for submodel_id in submodel_ids:
             submodel = shellsmith.get_submodel(submodel_id)
             id_short = submodel.get("idShort", "<no idShort>")
-            print(f"- {id_short} ({submodel_id})")
+            print(f"- {id_short}: {submodel_id}")
 
 
 def print_shells_tree():
     shells = shellsmith.get_shells()
     for shell in shells:
-        shell_id = shell['id']
-        shell_id_short = shell.get('idShort', "<no idShort>")
+        shell_id = shell["id"]
+        shell_id_short = shell.get("idShort", "<no idShort>")
         print(f"{shell_id_short}: {shell_id}")
 
         submodels = services.get_shell_submodels(shell_id)
@@ -33,9 +32,9 @@ def print_shells_tree():
         for i, submodel in enumerate(submodels):
             is_last = i == len(submodels) - 1
             prefix = "└──" if is_last else "├──"
-            submodel_id = submodel['id']
-            submodel_id_short = submodel.get('idShort', "<no idShort>")
-            print(f"{prefix} {submodel_id}: {submodel_id_short}")
+            submodel_id = submodel["id"]
+            submodel_id_short = submodel.get("idShort", "<no idShort>")
+            print(f"{prefix} {submodel_id_short}: {submodel_id}")
 
 
 def print_dangling_submodel_refs():
@@ -46,7 +45,7 @@ def print_dangling_submodel_refs():
         print("⚠️ Dangling Submodel References:")
         for shell_id, submodel_ids in dangling.items():
             shell = shellsmith.get_shell(shell_id)
-            shell_id_short = shell.get('idShort', "<no idShort>")
+            shell_id_short = shell.get("idShort", "<no idShort>")
             print(f"- {shell_id_short}: {shell_id}")
             for submodel_id in submodel_ids:
                 print(f"  └── {submodel_id}")
